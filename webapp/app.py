3973763bbadb1d6b484a171f370071df1039accf
vchraf <vchraf@gmail.com>
2022-10-06 22:53:47 +0100

    setup redis

---
+++ webapp/app.py
@@ -0,0 +1,36 @@
+import redis
+import hashlib
+import json
+from flask import Flask, jsonify,request
+
+app = Flask('webapp')
+
+redis_host = "localhost"
+redis_port = 6379
+redis_password = ""
+
+r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)   
+
+@app.route('/', methods=['POST'])
+def predict_endpoint():
+    _request = request.get_json()
+    key = hashlib.sha256(f"ss{json.dumps(_request)}".encode()).hexdigest()
+    print(key)
+    if r.exists(key):
+        val = r.get(key)
+    else:
+        val = _request['v1']**_request['v2']
+        r.set(key, val)
+    result = {
+        'prix': val #model.predict(list(_request.values()))
+    }
+    return jsonify(result)
+
+@app.route("/v", methods =["GET", "POST"])
+def visits():
+    v = int(r.get("v")) + 1
+    r.set("v", v)
+    return str(v)    
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9772)
